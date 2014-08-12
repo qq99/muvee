a66cd5b1b823139ef0ab947e74c94734af5567c3
Anthony Cameron <ar.cameron@gmail.com>
2014-08-11 22:30:21 -0400

    begin on series#show

--- app/controllers/series_controller.rb
+++ app/controllers/series_controller.rb
@@ -1,5 +1,16 @@
 class SeriesController < ApplicationController
+  before_action :set_series, only: [:show]
+
   def index
     @series = Series.all
   end
+
+  def show
+    render layout: 'fullscreen'
+  end
+
+  private
+    def set_series
+      @series = Series.find(params[:id])
+    end
 end
