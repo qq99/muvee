a231210988c5f08f128a17cab5cf8db531f3ade0
Anthony Cameron <ar.cameron@gmail.com>
2014-08-27 01:01:17 -0400

    adding an action to get sources for a movie based on IMDB id

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,5 @@
 class MoviesController < ApplicationController
-  before_action :set_movie, only: [:show]
+  before_action :set_movie, only: [:show, :find_sources]
 
   def index
     @movies = Movie.local.all
@@ -10,8 +10,13 @@ class MoviesController < ApplicationController
     @movies = Movie.remote.all
   end
 
+  def find_sources
+    @sources = TorrentManagerService.find_sources(@movie)
+    render partial: 'source_options', locals: {sources: @sources}
+  end
+
   private
     def set_movie
-      @series = Movie.find(params[:id])
+      @movie = Movie.find(params[:id])
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,9 @@ Rails.application.routes.draw do
     collection do
       get 'remote'
     end
+    member do
+      get 'find_sources'
+    end
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
