f72ae0dac763cb376ebc43780114da2b3719ea4e
Anthony Cameron <ar.cameron@gmail.com>
2014-09-21 18:21:37 -0400

    validate uniqueness of imdb_id for remote movies only

--- app/models/video.rb
+++ app/models/video.rb
@@ -8,6 +8,7 @@ class Video < ActiveRecord::Base
   has_many :torrents
 
   validates_uniqueness_of :raw_file_path, allow_nil: true, allow_blank: true
+  validates_uniqueness_of :imdb_id, allow_nil: true, allow_blank: true, conditions: -> { where.not(status: 'local') }
   after_create :shellout_and_grab_duration
   after_create :create_initial_thumb
 
