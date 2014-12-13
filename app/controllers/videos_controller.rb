86a2cc08cde554aaaa87fc1c82688393b892d108
Anthony Cameron <ar.cameron@gmail.com>
2014-12-13 01:16:17 -0500

    display a media count on superhome

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -6,6 +6,9 @@ class VideosController < ApplicationController
   def index
     @movies = Movie.local.all
     @series = Series.all
+    @n_movies = Movie.local.count
+    @n_series = Series.count
+    @n_shows = TvShow.count
     @items = @movies.to_a.concat @series.to_a
     @items.sort_by!{|item| -item.updated_at.to_i} # newest first
   end
