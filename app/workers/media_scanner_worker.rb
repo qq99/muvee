ab3aa656e3932df0cd82311a7e9082c04f13ab47
Anthony Cameron <ar.cameron@gmail.com>
2015-01-11 20:35:19 -0500

    clean missing before scanning for new

--- app/workers/media_scanner_worker.rb
+++ app/workers/media_scanner_worker.rb
@@ -3,7 +3,21 @@ class MediaScannerWorker
   sidekiq_options :retry => false
   sidekiq_options :queue => :scan
 
+  def clean_missing
+    klasses = [TvShow, Movie]
+    klasses.each do |klass|
+      klass.local.all.each do |model_instance|
+        if model_instance.local? && (!model_instance.raw_file_path || !File.exist?(model_instance.raw_file_path))
+          model_instance.destroy
+        end
+      end
+    end
+  end
+
   def perform
+
+    clean_missing
+
     config = ApplicationConfiguration.first
     return if config.blank?
     service = VideoCreationService.new({
