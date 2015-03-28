1eafb5635552dc45c94b43db4b012210dd938824
Anthony Cameron <ar.cameron@gmail.com>
2015-03-28 03:01:20 -0400

    fix tests, show the 3D-ness beside the quality

--- test/models/movie_source_test.rb
+++ test/models/movie_source_test.rb
@@ -3,18 +3,16 @@ require 'test_helper'
 class MovieSourceTest < ActiveSupport::TestCase
 
   def movie_stubbage
-    Movie.any_instance.stubs(:associate_with_genres)
-    Movie.any_instance.stubs(:create_initial_thumb)
-    Movie.any_instance.stubs(:shellout_and_grab_duration)
-    Movie.any_instance.stubs(:extract_metadata)
-    Movie.any_instance.stubs(:download_poster)
+    Movie.any_instance.expects(:associate_with_genres)
+    Movie.any_instance.expects(:create_initial_thumb)
+    Movie.any_instance.expects(:shellout_and_grab_duration)
+    Movie.any_instance.expects(:extract_metadata)
+    Movie.any_instance.expects(:download_poster)
   end
 
   test "#create will infer the quality for TvShows" do
-    movie_stubbage
     s = MovieSource.new(raw_file_path: '/foo/bar/True.Grit.2010.1080p.mp4')
     s.save
-
     assert_equal '1080p', s.quality
   end
 
@@ -31,6 +29,7 @@ class MovieSourceTest < ActiveSupport::TestCase
 
   test '#create for a new movie' do
     movie_stubbage
+    Movie.any_instance.expects(:search_for_imdb_id).once.returns("tt12345")
 
     s = MovieSource.new(raw_file_path: '/foo/bar/Synecdoche.NY.2008.1080p.mp4')
     assert_difference 'MovieSource.count', +1 do
