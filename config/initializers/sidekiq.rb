61db6e0c63ec7571e9152183372c5a546da7f7e8
Anthony Cameron <ar.cameron@gmail.com>
2015-08-06 22:59:25 -0400

    dont start sidekiq cron when rake/console

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,9 @@
 schedule_file = "config/schedule.yml"
 
-unless Rails.env.test?
+unless Rails.env.test? || $rails_rake_task || defined?(Rails::Console)
+  puts "Checking for job schedule file"
   if File.exists?(schedule_file)
+    puts "Running jobs on schedule"
     Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
   end
 end
