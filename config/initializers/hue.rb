8c70c23356334247b4711b6fd2cbdcc27d952d45
Anthony Cameron <ar.cameron@gmail.com>
2015-08-09 12:17:31 -0400

    rescue Hue::NoBridgeFound, fixes #20

--- config/initializers/hue.rb
+++ config/initializers/hue.rb
@@ -1,6 +1,11 @@
 HUE_CLIENT = if is_server_or_sidekiq_context?
   puts '=> initializers/hue: Initializing Hue client'
-  Hue::Client.new
+  begin
+    Hue::Client.new
+  rescue Hue::NoBridgeFound => e
+    puts '=> initializers/hue: Hue bridge not found'
+    nil
+  end
 else
   {}
 end
