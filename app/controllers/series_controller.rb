4b3a72c934bb751d84cb0c318366fc55d4131e17
Anthony Cameron <ar.cameron@gmail.com>
2015-11-07 13:39:36 -0500

    remove old dead cruft

--- app/controllers/series_controller.rb
+++ app/controllers/series_controller.rb
@@ -1,5 +1,5 @@
 class SeriesController < ApplicationController
-  before_action :set_series, only: [:show, :find_episode, :download, :reanalyze, :favorite, :unfavorite]
+  before_action :set_series, only: [:show, :download, :reanalyze, :favorite, :unfavorite]
   before_action :set_episode, only: [:show_episode_details, :download]
 
   RESULTS_PER_PAGE = 48
