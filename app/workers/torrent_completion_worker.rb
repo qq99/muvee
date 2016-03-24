638ddc1529224f8f1b629fdf4c0b4ea9a4669daa
Anthony Cameron <ar.cameron@gmail.com>
2016-03-24 14:10:09 -0400

    consult rails config when working of torrent jobs

--- app/workers/torrent_completion_worker.rb
+++ app/workers/torrent_completion_worker.rb
@@ -2,6 +2,8 @@ class TorrentCompletionWorker
   include Sidekiq::Worker
 
   def perform
+    return unless Rails.configuration.control_transmission
+    
     Torrent.all.each do |torrent|
       status = torrent.completion_status
       if status == "missing"
