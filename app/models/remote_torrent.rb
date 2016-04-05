506d5de6ecc68b5a83be3346022c15a536968400
Anthony Cameron <ar.cameron@gmail.com>
2016-04-04 21:45:18 -0400

    revamp torrent UX, let movies re-search

--- app/models/remote_torrent.rb
+++ app/models/remote_torrent.rb
@@ -1,7 +1,24 @@
 class RemoteTorrent < Hashie::Mash
+  GOOD_SEED_COUNT_THRESHOLD = (50).freeze
+  GOOD_RATIO_THRESHOLD = (0.5).freeze
 
   def ratio
     @ratio ||= (seeders.to_f / leechers.to_f) if seeders.present? && leechers.present?
+  end
+
+  def good_seeds?
+    seeders.present? &&
+      seeders > GOOD_SEED_COUNT_THRESHOLD
+  end
+
+  def good_ratio?
+    ratio.present? &&
+      ratio.finite? &&
+      ratio > GOOD_RATIO_THRESHOLD
+  end
+
+  def dead?
+    seeders.blank? || seeders == 0
   end
 
 end
